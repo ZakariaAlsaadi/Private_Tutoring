eed8a25fede3e84792df1389447307558bba57cb
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-05-27 00:31:58 +0300

    v0.0.2

--- index.js
+++ index.js
@@ -9,22 +9,31 @@ app.use(bodyParser.json());
 app.use(express.json());
 const port = process.env.PORT || 4040;
 const server = http.createServer(app);
-const token = "5232088474:AAHHrA2KbFkHH5VhrOn68QR-rDwxdECLU3o";
 
+const token = "5232088474:AAHHrA2KbFkHH5VhrOn68QR-rDwxdECLU3o";
+const apiUrl = `https://api.telegram.org/bot${token}`;
+const webHook = `${myServerUrl}webhook`;
 
 const database = require("./db/connect");
 
-// Start the server
+// Start the server webpage
 app.get('/', (req,res) => {
 
     database.query("SELECT * FROM teachers", function (err, result, fields) {
         if (err) throw err;
         res.send(result);
-        
       });
-
 });
 
+// Telegram Bot messages
+app.post("/webhook", async (req, res) => {
+    const { message } = req.body;
+    check_who_is_sending(message); // بدي شيك اذا هو استاذ او طالب وبأنو مرحلة 
+});
+
+function check_who_is_sending(message) {
+    console.log(message,"نكمل لاحقا");
+}
 
 // Start the server
 server.listen(port, () => {
