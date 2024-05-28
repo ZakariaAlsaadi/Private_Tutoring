bcaf3cbc5056392c3e0eed50d7a21ea0bb0d13ff
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-05-28 23:19:52 +0300

    v0.0.14

--- models/userModel.js
+++ models/userModel.js
@@ -2,15 +2,17 @@ const database = require("../db/connect");
 
  function getNewUser (chat) {
     database.query(`SELECT telegram_id FROM telegram_user WHERE telegram_id = ${chat.id}`,
-        function (err, result, fields) {
+        async function (err, result, fields) {
         if (err) throw err;
-        if (result == null) {
+        if (result == [null]) {
+            console.log('1');
             database.query(
-            `INSERT INTO 'telegram_user' ('id', 'user_name', 'first_name', 'last_name', 'telegram_id', 'teacher_profile_id') VALUES (NULL, '@${chat.username}', '${chat.first_name}', '${chat.last_name}', '${chat.id}', NULL;`
+                `INSERT INTO telegram_user (id, user_name, first_name, last_name, telegram_id, teacher_profile_id) VALUES (NULL, '@${chat.username}', '${chat.first_name}', '${chat.last_name}', '${chat.id}', NULL);`
             , function (err, result, fields) {
                 if (err) throw err;
                 console.log('added to the table');
             });
+            console.log('2')
         }
         console.log('success');
     });
