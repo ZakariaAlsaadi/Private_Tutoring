0665df56c6f7916efb901ea60f512921625fd1b5
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-05-28 23:30:31 +0300

    v0.0.16

--- models/userModel.js
+++ models/userModel.js
@@ -5,18 +5,14 @@ const database = require("../db/connect");
             function (err, result, fields) {
         if (err) throw err;
         if (result.length == 0) {
-            console.log('1');
             database.query(
                 `INSERT INTO telegram_user (id, user_name, first_name, last_name, telegram_id, teacher_profile_id) VALUES (NULL, '@${chat.username}', '${chat.first_name}', '${chat.last_name}', '${chat.id}', NULL);`
             , function (err, result, fields) {
                 if (err) throw err;
                 console.log('added to the table');
             });
-            console.log('2')
         }
         console.log('success');
     });
 }
-
-
 module.exports = {getNewUser}
