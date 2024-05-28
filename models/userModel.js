3d30b719482987fe09c7777ae5def69f5eb3b993
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-05-28 22:28:05 +0300

    v0.0.12

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,10 @@
 const database = require("../db/connect");
 
  function getNewUser (chat) {
-    database.query("SELECT telegram_id FROM telegram_user ", function (err, result, fields) {
+    database.query(`SELECT telegram_id FROM telegram_user 
+                    WHERE telegram_id = ${chat.id}`, function (err, result, fields) {
         if (err) throw err;
-        for (let i = 0; i < result.length; i++) {
-            if (result[i].telegram_id == chat.id) {
-              isExist = false;
-              break;
-            }
-        }
-        if (isExist == false) {
+        if (result == null) {
             database.query(
             `INSERT INTO 'telegram_user' ('id', 'user_name', 'first_name', 'last_name', 'telegram_id', 'teacher_profile_id') 
             VALUES (NULL, '@${chat.username}', '${chat.first_name}', '${chat.last_name}', '${chat.id}', NULL;`
