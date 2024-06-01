da74b082d1e05587f182e03f4fd8ea45f8230a7c
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-06-02 01:03:03 +0300

    v0.0.59

--- models/teacherModel.js
+++ models/teacherModel.js
@@ -7,32 +7,6 @@ function getTeachers () {
         return result;
       });
 }
-/*
-function updateFirstName (text,telegramId) {
-    database.query(`UPDATE teachers
-    SET first_name = '${text}'
-    WHERE telegram_id = ${telegramId};
-    `, function (err, result, fields) {
-        if (err) throw err;
-        console.log('success');
-      });
-
-}
-*/
-function updateSignUp (message,step_number) {
-  step_number = Math.min(step_number,10);
-  database.query(`SELECT * FROM sign_up_steps`, function (err, teacher_step, fields) 
-  {  
-    if (err) throw err;
-            database.query(
-              `UPDATE teachers SET ${teacher_step[step_number].the_step} = '${message.text}' , sign_up_step = ${step_number} WHERE telegram_id = ${message.chat.id};`
-            , function (err, result, fields) 
-      {
-            if (err) throw err;
-            console.log('success');
-      });
-  });
-}
 
 function askForSignUpInfo (message) {
   database.query(`SELECT * FROM teachers WHERE telegram_id = ${message.chat.id};`
--- telegram.js
+++ telegram.js
@@ -6,26 +6,7 @@ class TelegramBot {
     constructor() {
         this.botToken = process.env.bot_token;
     }
-/*
-    async sendMessage(message, chatId) {
-        const url = `${TELEGRAM_API_URL}${this.botToken}/sendMessage`;
-
-        try {
-            const response = await axios.post(url, {
-                chat_id: chatId,
-                text: message
-            });
-
-            if (!response.data.ok) {
-                throw new Error(`Error sending message: ${response.data.description}`);
-            }
-
-            return response.data.result;
-        } catch (error) {
-            throw new Error(`Error sending message: ${error.message}`);
-        }
-    }
-*/
+    
     async getUpdates() {
         const url = `${TELEGRAM_API_URL}${this.botToken}/getUpdates?offset=${this.offset}`;
 
