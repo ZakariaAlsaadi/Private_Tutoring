63ba73e9a703f6cf4cda9e6aeacd8c77bfdc0a6d
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-06-02 00:53:24 +0300

    v0.0.57

--- models/telegramButtonsModel.js
+++ models/telegramButtonsModel.js
@@ -30,22 +30,32 @@ async function telegramButtons (message, step_number) {
         }
 
         else if (step_number == 2) {
-            if (message.text == "ذكر" || message.text == "انثى") {
-                telegramBot.sendMessage(
-                    "ما هو اعلى مرتب من الممكن ان تأخذه في الساعة",
-                    message.from.id
-                  );
-                updateSignUp(message, step_number);
-            }
-    
-            else {
-            const replyKeyboard = {
-                keyboard: [[{ text: "انثى" }, { text: "ذكر" }]],
-                resize_keyboard: true,
-              };
-              await telegramBot.sendMessage("هل انت ذكر ام انثى ؟", message.chat.id, replyKeyboard);
-            }
+          const replyKeyboard = {
+              keyboard: [[{ text: "انثى" }, { text: "ذكر" }]],
+              resize_keyboard: true,
+            };
+            await telegramBot.sendMessage("هل انت ذكر ام انثى ؟", message.chat.id, replyKeyboard);
+            updateSignUp(message, step_number);
+          }
+
+        else if (step_number == 3) {
+                if (message.text == "ذكر" || message.text == "انثى") {
+                    telegramBot.sendMessage(
+                        "ما هو اعلى مرتب من الممكن ان تأخذه في الساعة",
+                        message.from.id
+                      );
+                    updateSignUp(message, step_number);
+                }
+                else {
+                  const replyKeyboard = {
+                    keyboard: [[{ text: "انثى" }, { text: "ذكر" }]],
+                    resize_keyboard: true,
+                  };
+                  await telegramBot.sendMessage("هل انت ذكر ام انثى ؟", message.chat.id, replyKeyboard);
+                }
         }
+        
+        
 
 
 }
