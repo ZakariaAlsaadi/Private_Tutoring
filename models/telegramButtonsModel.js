eb75e94205605483a5f56f0d744bdae5d04e55d1
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-06-02 01:05:39 +0300

    v0.0.60

--- models/telegramButtonsModel.js
+++ models/telegramButtonsModel.js
@@ -59,57 +59,5 @@ async function telegramButtons (message, step_number) {
 
 
 }
-/*
-    async telegramButtons0 (message) {
-        telegramBot.sendMessage("ما هو لقبك (الكنية)", message.from.id);
-        teacherModel.updateSignUp(message, 0);
-    }
-    async telegramButtons1 (message) {
-        telegramBot.sendMessage("ما هو رقم الهاتف ؟", message.from.id);
-        teacherModel.updateSignUp(message, 1);
-    }
-    async telegramButtons2 (message) {
 
-        if (message.text == "ذكر" || message.text == "انثى") {
-            telegramBot.sendMessage(
-                "ما هو اعلى مرتب من الممكن ان تأخذه في الساعة",
-                message.from.id
-              );
-            teacherModel.updateSignUp(message, 2);
-        }
-
-        else {
-        const replyKeyboard = {
-            keyboard: [[{ text: "انثى" }, { text: "ذكر" }]],
-            resize_keyboard: true,
-          };
-          await telegramBot.sendMessage("هل انت ذكر ام انثى ؟", message, replyKeyboard);
-        }
-    }
-    async telegramButtons3 (message) {
-        
-    }
-    async telegramButtons4 (message) {
-        
-    }
-    async telegramButtons5 (message) {
-        
-    }
-    async telegramButtons6 (message) {
-        
-    }
-    async telegramButtons7 (message) {
-        
-    }
-    async telegramButtons8 (message) {
-        
-    }
-    async telegramButtons9 (message) {
-        
-    }
-    async telegramButtons10 (message) {
-        
-    }
-}
-*/
 module.exports = {telegramButtons};
