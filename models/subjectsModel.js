1f47dd04daa6af636ec34c25fb73fe47cc310467
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-06-02 20:32:46 +0300

    v0.0.69

--- models/subjectsModel.js
+++ models/subjectsModel.js
@@ -1,16 +1,26 @@
-async function subjectsButtons () {
-    database.query(`SELECT subject FROM subjects`, async function (err, subjectList, fields) 
+async function subjectsButtons (askingForSubjectOrClass, message, subjectOrClass) {
+
+    if (subjectOrClass == "subject") {
+        sqlTable = 'subjects';
+        sqlColumn = 'subject';
+    }
+    else {
+        sqlTable = 'classes';
+        sqlColumn = 'class_name';
+    }
+
+    database.query(`SELECT ${sqlColumn} FROM ${sqlTable}`, async function (err, subjectOrClassList, fields) 
     {  
       if (err) throw err;
             
     var KeyBoardRow = [];
     var keyBoard = [];
-    for (let j = 0; j < subjectList.length; j++) {
+    for (let j = 0; j < subjectOrClassList.length; j++) {
       if (j % 3 == 0) {
         keyBoard.push(KeyBoardRow);
         KeyBoardRow = [];
       }
-      KeyBoardRow.push({ text: `${subjectList[j].subject}` });
+      KeyBoardRow.push({ text: `${subjectOrClassList[j]}` });
     }
     if (KeyBoardRow.length != 0) {
       keyBoard.push(KeyBoardRow);
@@ -20,7 +30,7 @@ async function subjectsButtons () {
       keyboard: keyBoard,
       resize_keyboard: true,
     };
-    await telegramBot.sendMessage(textStep, message.chat.id, replyKeyboard);
+    await telegramBot.sendMessage(askingForSubjectOrClass, message.chat.id, replyKeyboard);
     });
 }
 
