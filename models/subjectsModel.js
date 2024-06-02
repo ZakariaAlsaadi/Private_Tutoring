a62dbf59e0700ee4f438eb9eeb241e6fc24e6d8b
Zakaria Al-saadi <abohl2dm@gmail.com>
2024-06-02 21:10:48 +0300

    v0.0.74

--- models/subjectsModel.js
+++ models/subjectsModel.js
@@ -5,19 +5,7 @@ const telegramBot = new TelegramBot;
 async function subjectsButtons (askingForSubjectOrClass, message, subjectOrClass) {
 
     if (subjectOrClass == "subject") {
-        sqlTable = 'subjects';
-        sqlColumn = 'subject';
-    }
-    else if (subjectOrClass == "subject") {
-        sqlTable = 'classes';
-        sqlColumn = 'class_name';
-    }
-    else {
-        console.log('error , you didnt choose subject or class');
-        return 0 ;
-    }
-
-    database.query(`SELECT ${sqlColumn} FROM ${sqlTable}`, async function (err, subjectOrClassList, fields) 
+        database.query(`SELECT subject FROM subjects`, async function (err, subjectOrClassList, fields) 
     {  
       if (err) throw err;
             
@@ -28,7 +16,7 @@ async function subjectsButtons (askingForSubjectOrClass, message, subjectOrClass
         keyBoard.push(KeyBoardRow);
         KeyBoardRow = [];
       }
-      KeyBoardRow.push({ text: `${subjectOrClassList[j]}` });
+      KeyBoardRow.push({ text: `${subjectOrClassList[j].subject}` });
     }
     if (KeyBoardRow.length != 0) {
       keyBoard.push(KeyBoardRow);
@@ -40,6 +28,37 @@ async function subjectsButtons (askingForSubjectOrClass, message, subjectOrClass
     };
     await telegramBot.sendMessage(askingForSubjectOrClass, message.chat.id, replyKeyboard);
     });
+
+    }
+    else if (subjectOrClass == "subject") {
+        database.query(`SELECT class_name FROM classes`, async function (err, subjectOrClassList, fields) 
+    {  
+      if (err) throw err;
+            
+    var KeyBoardRow = [];
+    var keyBoard = [];
+    for (let j = 0; j < subjectOrClassList.length; j++) {
+      if (j % 3 == 0) {
+        keyBoard.push(KeyBoardRow);
+        KeyBoardRow = [];
+      }
+      KeyBoardRow.push({ text: `${subjectOrClassList[j].class_name}` });
+    }
+    if (KeyBoardRow.length != 0) {
+      keyBoard.push(KeyBoardRow);
+      KeyBoardRow = [];
+    }
+    const replyKeyboard = {
+      keyboard: keyBoard,
+      resize_keyboard: true,
+    };
+    await telegramBot.sendMessage(askingForSubjectOrClass, message.chat.id, replyKeyboard);
+    });
+    }
+    else {
+        console.log('error , you didnt choose subject or class');
+        return 0 ;
+    }
 }
 
 function insertSubjects (message) {
